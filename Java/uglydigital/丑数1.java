6ded224dd607ba846d79ea93af99bc6cb93b078e
sharklee2017 <30565262+sharklee2017@users.noreply.github.com>
2017-09-26 11:42:08 +0800

    Update 丑数1.java

--- Java/uglydigital/丑数1.java
+++ Java/uglydigital/丑数1.java
@@ -9,22 +9,10 @@
      * @return true if num is an ugly number or false
      */
     public boolean isUgly(int num) {
-        // Write your code here
-        if(num != 0){
-       while (num % 2 == 0){
-           num /= 2;
-       }
-       while (num % 3 == 0){
-           num /= 3;
-       }
-       while (num % 5 ==0){
-           num /=5;
-       }
-       if( num ==1)
-          return true;
-       else 
-         return false;
-    }
-     return false;
+       public static boolean isUglyDigital(int num) {
+        for (int i=2; i<6 && num>0; i++)
+            while (num % i == 0)
+                num /= i;
+        return num == 1;
     }
 }
